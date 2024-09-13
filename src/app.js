9c93e6e35a3c5e5dce66752a1396308eb5852782
liha-engineer <liha.engineer@gmail.com>
2024-09-14 01:51:08 +0900

    feat: useritems 라우터 작성

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import AccountRouter from './routes/account.router.js';
 import CharactersRouter from './routes/characters.router.js';
 import ItemsRouter from './routes/items.router.js';
+import UserItemsRouter from './routes/useritems.router.js';
 import LogMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 import dotenv from 'dotenv'
@@ -15,7 +16,7 @@ app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', [AccountRouter, CharactersRouter, ItemsRouter]);
+app.use('/api', [AccountRouter, CharactersRouter, ItemsRouter, UserItemsRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(+process.env.PORT, () => {

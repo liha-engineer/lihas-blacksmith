0708445039bac0d789c50b7d07a5215c333ecfcc
liha-engineer <liha.engineer@gmail.com>
2024-09-22 00:16:29 +0900

    feat: import 구문 정렬

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import AccountRouter from './routes/account.router.js';
 import CharactersRouter from './routes/characters.router.js';
 import ItemsRouter from './routes/items.router.js';
-import UserItemsRouter from './routes/useritems.router.js';
+import InventoryRouter from './routes/inventory.router.js';
 import LogMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
 import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -16,7 +16,7 @@ app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', [AccountRouter, CharactersRouter, ItemsRouter, UserItemsRouter]);
+app.use('/api', [AccountRouter, CharactersRouter, ItemsRouter, InventoryRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(+process.env.PORT, () => {

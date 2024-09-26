16d17045ebdef9bfb52ead9731ee1488c12333f8
liha-engineer <liha.engineer@gmail.com>
2024-09-26 17:01:20 +0900

    feat: DB명 변경에 따른 import 경로 변경사항 커밋

--- src/utils/prisma/index.js
+++ src/utils/prisma/index.js
@@ -1,5 +1,5 @@
-import { PrismaClient as GameDataClient } from '../../../prisma/gamedb/generated/gameDataClient/index.js';
-import { PrismaClient as UserDataClient } from '../../../prisma/userdb/generated/userDataClient/index.js';
+import { PrismaClient as GameDataClient } from '../../../prisma/game/generated/gameDataClient/index.js';
+import { PrismaClient as UserDataClient } from '../../../prisma/user/generated/userDataClient/index.js';
 
 export const gameDataClient = new GameDataClient({
   log: ['query', 'info', 'warn', 'error'],

eb1a45d78543923bf29461b25586d0ce537148f3
liha-engineer <liha.engineer@gmail.com>
2024-10-01 02:29:31 +0900

    feat: 추가된 라우터 import

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import AccountRouter from './routes/account.router.js';
-import CharactersRouter from './routes/characters.router.js';
-import ItemsRouter from './routes/items.router.js';
-import InventoryRouter from './routes/inventory.router.js';
+import accountRouter from './routes/accounts.router.js';
+import charactersRouter from './routes/characters.router.js';
+import itemsRouter from './routes/items.router.js';
+import inventoryRouter from './routes/inventory.router.js';
+import merchantRouter from './routes/merchant.router.js';
+import equipmentsRouter from './routes/equipments.router.js';
 import LogMiddleware from './middlewares/log.middleware.js';
 import ErrorHandlingMiddleware from './middlewares/error-handling.middleware.js';
+
 import dotenv from 'dotenv'
 
 dotenv.config();
@@ -16,7 +19,7 @@ app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', [AccountRouter, CharactersRouter, ItemsRouter, InventoryRouter]);
+app.use('/api', [accountRouter, charactersRouter, itemsRouter, inventoryRouter, merchantRouter, equipmentsRouter]);
 app.use(ErrorHandlingMiddleware);
 
 app.listen(+process.env.PORT, () => {

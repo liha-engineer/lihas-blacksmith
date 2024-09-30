e20fec606a96394d99d5bb92de575aad6421a8de
liha-engineer <liha.engineer@gmail.com>
2024-09-30 14:20:16 +0900

    feat: 인증 미들웨어 에러처리 로직 수정 (throw new Error -> res.status)

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,29 +9,29 @@ const loginAuth = async (req, res, next) => {
   try {
     const authHeader = req.headers['authorization'];
     if (!authHeader)
-      throw new Error('토큰이 존재하지 않습니다');
+      return res.status(401).json({message : '토큰이 존재하지 않습니다.'})
 
     const token = authHeader.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.ACESSTOKEN_KEY);
     if (!decodedToken)
-        throw new Error('유효하지 않은 토큰입니다');
+      return res.status(401).json({message : '유효하지 않은 토큰입니다.'})
 
-    // 토큰만들때 accountId 저장하게 해서 만들었다. 
-    // 그래서 검증완료된 토큰에서 그거 꺼내 쓸 것임
+    // 토큰만들때 accountId 저장하게 해서 만들었으므로, 검증완료된 토큰에서 꺼내 쓸것
     const accountId = decodedToken.accountId;
     const user = await userDataClient.account.findFirst ({
         where : { accountId : +accountId }, 
     }); 
-    if (!user) throw new Error ("토큰 사용자가 존재하지 않습니다")
+    if (!user)
+      return res.status(401).json({message : '토큰 사용자가 존재하지 않습니다.'})
 
     // req.user에 조회된 사용자 정보를 할당
     req.user = user;
     next();
   } catch (err) {
     if (err.name === 'TokenExpiredError')
-        return res.status(401).json({message : '토큰이 만료되었습니다.'})
+      return res.status(401).json({message : '토큰이 만료 되었습니다.'})
     if (err.name === 'JsonWebTokenError')
-        return res.status(401).json({message : '토큰이 손상되었습니다.'})
+        return res.status(401).json({message : '토큰이 손상 되었습니다.'})
 
     return res.status(400).json({ message: err.message });
   }

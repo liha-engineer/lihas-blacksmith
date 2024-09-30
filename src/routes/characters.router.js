7fd8fafcc2dadc85e800564ae3dc54e826062a8f
liha-engineer <liha.engineer@gmail.com>
2024-10-01 02:24:15 +0900

    feat: 캐릭터 라우터 주석 정리

--- src/routes/characters.router.js
+++ src/routes/characters.router.js
@@ -6,10 +6,6 @@ import { Prisma } from '@prisma/client';
 
 const router = express.Router();
 
-// 캐릭터 생성하려는 사용자가 인증된 사용자인지 검사
-// 캐릭터 생성을 위해 캐릭터명을 body로 전달할것
-// Characters 테이블에 캐릭터를 생성
-// 이거 transaction 걸어서 오류났을때 캐릭터 슬롯만 차지하는 일 없어야 할듯
 router.post('/characters', authMiddleWare, async (req, res, next) => {
   try {
     const { characterName } = req.body;
@@ -18,8 +14,7 @@ router.post('/characters', authMiddleWare, async (req, res, next) => {
     const duplicateName = await userDataClient.characters.findFirst({
       where: { characterName },
     });
-    if (duplicateName)
-      return res.status(409).json({ message: '해당 이름의 캐릭터가 이미 존재합니다' });
+    if (duplicateName) return res.status(409).json({ message: '해당 이름의 캐릭터가 이미 존재합니다' });
 
     const result = await userDataClient.$transaction(
       async (tx) => {
@@ -67,62 +62,54 @@ router.get('/characters', authMiddleWare, async (req, res, next) => {
   return res.status(200).json({ message: '캐릭터 조회결과', data: characters });
 });
 
-// 캐릭터 상세조회 - 비로그인이거나 해당유저가 아닌 경우 money 및 상세정보 미출력하도록
-router.get(
-  '/characters/:characterId',
-  headerCheckMiddleWare,
-  authMiddleWare,
-  async (req, res, next) => {
-    const { characterId } = req.params;
-    const { accountId } = req.user;
-
-    // 지금 로그인해있는 유저랑 이 캐릭터의 유저랑 같은지 찾는것.
-    const isSameUser = await userDataClient.characters.findUnique({
-      where: { characterId: +characterId },
-    });
-
-    if (!isSameUser) return res.status(404).json({ message: '캐릭터가 존재하지 않습니다.' });
-
-    let characters = await userDataClient.characters.findUnique({
-      where: { characterId: +characterId },
-      select: {
-        characterId: true,
-        accountId: true,
-        characterName: true,
-        hp: true,
-        atk: true,
-        money: true,
-        createdAt: true,
-      },
-    });
-
-    // hp랑 atk만 보여주게 설정
-    if (accountId !== isSameUser.accountId) {
-      characters = await userDataClient.characters.findUnique({
-        where: { characterId: +characterId },
-        select: {
-          characterName: true,
-          hp: true,
-          atk: true,
-        },
-      });
-    }
-
-    return res.status(200).json({ data: characters });
+const otherCharacterInfo = {
+  select: {
+    characterName: true,
+    hp: true,
+    atk: true,
   },
-);
+};
 
-// 이 라우터는 상단의 headerCheckMiddleWare에 의해 헤더가 비어있으면(토큰 없으면) 여기로 건너뛰게 설정
-// 얘는 특별한 때만 돌아가는 녀석이다
-router.get('/characters/search', async (req, res, next) => {
-  const { characterName } = req.body;
-  const characters = await userDataClient.characters.findFirst({
-    where: { characterName },
+// 캐릭터 상세조회 - 비로그인이거나 해당유저가 아닌 경우 money 및 상세정보 미출력
+router.get('/characters/:characterId', headerCheckMiddleWare, authMiddleWare, async (req, res, next) => {
+  const { characterId } = req.params;
+  const { accountId } = req.user;
+
+  const character = await userDataClient.characters.findUnique({
+    where: { characterId: +characterId },
+  });
+
+  if (!character) return res.status(404).json({ message: '캐릭터가 존재하지 않습니다.' });
+
+  let characters = await userDataClient.characters.findUnique({
+    where: { characterId: +characterId },
     select: {
+      characterId: true,
+      accountId: true,
       characterName: true,
       hp: true,
       atk: true,
+      money: true,
+      createdAt: true,
     },
+  });
+
+  if (accountId !== character.accountId) {
+    characters = await userDataClient.characters.findUnique({
+      where: { characterId: +characterId },
+      ...otherCharacterInfo,
+    });
+  }
+
+  return res.status(200).json({ data: characters });
+});
+
+// 이 라우터는 상단의 headerCheckMiddleWare에 의해 헤더가 비어있으면(토큰 없으면) 여기로 건너뛰게 설정
+router.get('/characters/:characterId', async (req, res, next) => {
+  const { characterId } = req.params;
+  const characters = await userDataClient.characters.findFirst({
+    where: { characterId: +characterId },
+    ...otherCharacterInfo,
   });
   return res.status(200).json({ data: characters });
 });
@@ -136,16 +123,10 @@ router.delete('/characters/:characterId', authMiddleWare, async (req, res, next)
 
     const character = await userDataClient.characters.findFirst({
       where: { characterId: +characterId },
-      select: {
-        characterId: true,
-        characterName: true,
-        createdAt: true,
-      },
     });
-
     if (!character) return res.status(404).json({ message: '캐릭터가 존재하지 않습니다' });
 
-    const deleteCharacter = await userDataClient.characters.delete({
+    await userDataClient.characters.delete({
       where: { characterId: +characterId },
     });
 
